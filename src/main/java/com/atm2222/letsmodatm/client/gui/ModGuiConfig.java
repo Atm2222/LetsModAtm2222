1ee78d4281254bb38367a2428e0c2eb09fedafa7
Andrew Meckert <Andrew Meckert>
2016-08-10 21:15:05 -0700

    Coudlnt get Config GUI's to work, and couldnt get Pahimar's epsiode 7.5 fix working. Implemented Logging

---
+++ src/main/java/com/atm2222/letsmodatm/client/gui/ModGuiConfig.java
@@ -0,0 +1,20 @@
+package com.atm2222.letsmodatm.client.gui;
+
+import com.atm2222.letsmodatm.handler.ConfigurationHandler;
+import com.atm2222.letsmodatm.reference.Reference;
+import cpw.mods.fml.client.config.GuiConfig;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.common.config.ConfigElement;
+import net.minecraftforge.common.config.Configuration;
+
+
+public class ModGuiConfig extends GuiConfig{
+    public ModGuiConfig(GuiScreen guiScreen) {
+        super(guiScreen,
+                new ConfigElement(ConfigurationHandler.configuration.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements(),
+                Reference.MOD_ID,
+                false,
+                false,
+                GuiConfig.getAbridgedConfigPath(ConfigurationHandler.configuration.toString()));
+    }
+}
--- src/main/java/com/atm2222/letsmodatm/reference/Reference.java
+++ src/main/java/com/atm2222/letsmodatm/reference/Reference.java
@@ -2,6 +2,9 @@ package com.atm2222.letsmodatm.reference;
 
 public class Reference {
     public static final String MOD_ID = "LetsModAtm";
-    public static final String Mod_Name = "LetsModAtm";
+    public static final String MOD_NAME = "LetsModAtm";
     public static final String VERSION = "1.7.10-1.0";
+    public static final String CLIENT_PROXY_CLASS = "com.atm2222.letsmodatm.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "com.atm2222.letsmodatm.proxy.ServerProxy";
+    public static final String GUI_FACTORY_CLASS = "com.atm2222.letsmodatm.client.gui.GuiFactory";
 };
